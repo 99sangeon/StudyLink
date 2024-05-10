39c1a8634d1364da1a2537c687502b9d02872104
tkd02 <tkd0204@naver.com>
2024-05-10 18:10:52 +0900

    feat : Error 공통 응답 포맷 구현

---
+++ src/main/java/com/project/studylink/dto/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.project.studylink.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.project.studylink.enums.ErrorCode;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> fields;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.code = errorCode.name();
+        this.message = errorCode.getMessage();
+    }
+
+    public ErrorResponse(ErrorCode errorCode, Map<String, String> fields) {
+        this.code = errorCode.name();
+        this.message = errorCode.getMessage();
+        this.fields = fields;
+    }
+}
