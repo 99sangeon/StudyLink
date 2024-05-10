280bbb3a6246f1c2bf8ca34c256cb475f6382775
tkd02 <tkd0204@naver.com>
2024-05-10 18:27:00 +0900

    feat : FIELD_ERROR 상수 추가

--- src/main/java/com/project/studylink/enums/ErrorCode.java
+++ src/main/java/com/project/studylink/enums/ErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    FIELD_ERROR(HttpStatus.BAD_REQUEST, "입력하신 내용을 다시 확인해주세요."),
+
     NOT_FOUND_RESOURCE(HttpStatus.NOT_FOUND, "요청하신 url을 찾을 수 없습니다."),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 오류가 발생했습니다.");
