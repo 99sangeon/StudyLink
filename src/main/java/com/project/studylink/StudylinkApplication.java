005b3a9187f8b44a1ecb4f2be7e65f208bbb85a1
tkd02 <tkd0204@naver.com>
2024-05-10 16:32:07 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/studylink/StudylinkApplication.java
@@ -0,0 +1,13 @@
+package com.project.studylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudylinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudylinkApplication.class, args);
+	}
+
+}
