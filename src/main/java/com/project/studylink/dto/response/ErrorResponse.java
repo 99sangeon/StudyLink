5c68fa7e640dfaaa04fca9d0950292a40c174fbf
tkd02 <tkd0204@naver.com>
2024-05-13 19:08:03 +0900

    refactor : dto/response 디렉토리로 이동

--- src/main/java/com/project/studylink/dto/response/ErrorResponse.java
+++ src/main/java/com/project/studylink/dto/response/ErrorResponse.java
@@ -1,4 +1,4 @@
-package com.project.studylink.dto;
+package com.project.studylink.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.project.studylink.enums.ErrorCode;
