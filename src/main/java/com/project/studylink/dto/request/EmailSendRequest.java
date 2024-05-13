4181661a282969674c0e959b300092291ae1f540
tkd02 <tkd0204@naver.com>
2024-05-13 19:09:52 +0900

    feat : 이메일 전송 DTO 생성

---
+++ src/main/java/com/project/studylink/dto/request/EmailSendRequest.java
@@ -0,0 +1,19 @@
+package com.project.studylink.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmailSendRequest {
+
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    private String email;
+}
